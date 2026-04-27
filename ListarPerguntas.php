d3e7f3d08e8938ad5048d9c4081b03cd6ffbadc2
RaphaelTrovao <raphaeltrovao123@gmail.com>
2026-04-27 07:18:03 -0300

    alterações no script de listar perguntas

--- ListarPerguntas.php
+++ ListarPerguntas.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+    <head></head>
     <body>
         <table>
             <tr><th>Pergunta</th><th>Resposta</th></tr>
@@ -11,7 +12,28 @@
  
  
         echo "<tr><td>" . $colunaDados[0] . "</td>" .
-            "<td>" . $colunaDados[1] . "<button>alterar</button> <button>excluir</button></td>" ;
+            "<td>" . $colunaDados[1] . "</td>" .
+            "<td>" . $colunaDados[2] . "</td>" .
+            "<td>" . $colunaDados[3] . "</td>" .
+            "<td>" . $colunaDados[4] . "</td>" .
+            "<td>" . $colunaDados[5] . " <button>alterar</button> <button>remover</button></td> </tr>" ;
+           
+    }
+ 
+   fclose($arq);
+            ?> 
+        </table>
+         <table>
+            <tr><th>Pergunta</th><th>Resposta</th></tr>
+            <?php
+            $arq = fopen("perguntaDisc.txt", "r") or die("erro");
+            while(!feof($arq)) {
+        $linha = fgets($arq);
+        $colunaDados = explode(";", $linha);
+ 
+ 
+        echo "<tr><td>" . $colunaDados[0] . "</td>" .
+            "<td>" . $colunaDados[1] . " <button>alterar</button> <button>remover</button></td> </tr>" ;
            
     }
  
